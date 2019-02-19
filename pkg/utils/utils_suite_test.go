c4c4eefe8afce34c8572aada19537240e79ffabd
Przemyslaw Lal <przemyslawx.lal@intel.com>
2019-02-19 10:00:54 +0000

    add tests for the utils package

---
+++ pkg/utils/utils_suite_test.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestUtils(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Utils Suite")
+}
