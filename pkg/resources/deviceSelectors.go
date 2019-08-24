075f28621756c431858835092637eabfd60a4f9b
Arkady Gilinsky <arkady.gilinsky@harmonicinc.com>
2019-08-24 13:49:56 +0300

    Beautify code style 2
    Signed-off-by: Arkady Gilinsky <arkady.gilinsky@harmonicinc.com>

--- pkg/resources/deviceSelectors.go
+++ pkg/resources/deviceSelectors.go
@@ -1,8 +1,8 @@
 package resources
 
 import (
-	"github.com/intel/sriov-network-device-plugin/pkg/types"
 	"fmt"
+	"github.com/intel/sriov-network-device-plugin/pkg/types"
 	"strconv"
 	"strings"
 )
@@ -81,19 +81,19 @@ func (s *pfNameSelector) Filter(inDevices []types.PciNetDevice) []types.PciNetDe
 	for _, dev := range inDevices {
 		selector := getItem(s.pfNames, dev.GetPFName())
 		if selector != "" {
-			if strings.Contains(selector, "#"){
+			if strings.Contains(selector, "#") {
 				// Selector does contain VF index in next format:
 				// <PFName>#<VFIndexStart>-<VFIndexEnd>
 				// In this case both <VFIndexStart> and <VFIndexEnd>
 				// are included in range, for example: "netpf0#3-5"
 				// The VFs 3,4 and 5 of the PF 'netpf0' will be included
 				// in selector pool
-				fields := strings.Split(selector,"#")
+				fields := strings.Split(selector, "#")
 				if len(fields) != 2 {
 					fmt.Printf("Failed to parse %s PF name selector, probably incorrect separator character usage\n", dev.GetPFName())
 					continue
 				}
-				rng := strings.Split(fields[1],"-")
+				rng := strings.Split(fields[1], "-")
 				if len(rng) != 2 {
 					fmt.Printf("Failed to parse %s PF name selector, probably incorrect range character usage\n", dev.GetPFName())
 					continue
@@ -149,11 +149,10 @@ func contains(hay []string, needle string) bool {
 	return false
 }
 
-
 func getItem(hay []string, needle string) string {
 	for _, item := range hay {
 		if strings.HasPrefix(item, needle) {
-			return  item
+			return item
 		}
 	}
 	return ""
