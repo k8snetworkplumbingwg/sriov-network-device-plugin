af734451506ffadd6c2024eb29736685092f1742
Martin Kennelly <mkennell@redhat.com>
2021-07-26 18:10:53 +0100

    Remove legacy backticks & prevent globbing/splitting
    Signed-off-by: Martin Kennelly <mkennell@redhat.com>

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -22,8 +22,8 @@ usage()
 }
 
 while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
+    PARAM="$(echo $1 | awk -F= '{print $1}')"
+    VALUE="$(echo $1 | awk -F= '{print $2}')"
     case $PARAM in
         -h | --help)
             usage
@@ -67,4 +67,4 @@ if [ "$CONFIG_FILE" != "" ]; then
     CLI_PARAMS="$CLI_PARAMS --config-file $CONFIG_FILE"
 fi
 
-exec $SRIOV_DP_SYS_BINARY_DIR/sriovdp $CLI_PARAMS
+exec "$SRIOV_DP_SYS_BINARY_DIR/sriovdp" "$CLI_PARAMS"
